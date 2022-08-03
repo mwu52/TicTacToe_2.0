e14a480f173869fb167a92ab8839973b422e652c
Michael Wu <mwu52@uic.edu>
2022-08-03 00:25:27 -0500

    players

---
+++ app.js
@@ -0,0 +1,6 @@
+function submitName(){
+    let player1 = document.getElementById("p1").value;
+    let player2 = document.getElementById("p2").value;
+    
+    document.getElementById("player1").innerText = "Player 1: " + player1;
+}
